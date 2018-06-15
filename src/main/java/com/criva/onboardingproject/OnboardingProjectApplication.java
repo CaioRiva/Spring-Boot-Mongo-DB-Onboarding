31aa4d77fde70289050b1170b28990ac655522cf
caioriva <caioriva@gmail.com>
2018-06-15 13:27:53 -0300

    Project setup and User entity creation

---
+++ src/main/java/com/criva/onboardingproject/OnboardingProjectApplication.java
@@ -0,0 +1,12 @@
+package com.criva.onboardingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnboardingProjectApplication.class, args);
+    }
+}
